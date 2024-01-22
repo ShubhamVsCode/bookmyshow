b19f269febdb006471e5fd13855345bc1307f9b6
Shubham <shubhamvscode@gmail.com>
2024-01-22 12:33:35 +0530

    refactor: updated homepage and some refactoring - issue: no-id

---
+++ src/app/booking/page.tsx
@@ -0,0 +1,7 @@
+import AdminLayout from "@/components/admin-layout";
+
+const BookingPage = () => {
+  return <AdminLayout />;
+};
+
+export default BookingPage;
--- src/middleware.ts
+++ src/middleware.ts
@@ -2,21 +2,21 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { ACCESS_TOKEN } from "./constants/common.constants";
 
-const protectedRoute = ["/"];
+// const protectedRoute = ["/"];
 
 export function middleware(request: NextRequest): NextResponse | undefined {
-  if (protectedRoute.includes(request.nextUrl.pathname)) {
-    const user = request.cookies.has(ACCESS_TOKEN);
-    if (!user) {
-      const { href, origin } = request.nextUrl;
-      const url = new URL(
-        `${origin}/login?redirectUrl=${encodeURIComponent(
-          `${href.replace(origin, "")}`
-        )}`
-      );
-      return NextResponse.redirect(url);
-    }
-  }
+  // if (protectedRoute.includes(request.nextUrl.pathname)) {
+  //   const user = request.cookies.has(ACCESS_TOKEN);
+  //   if (!user) {
+  //     const { href, origin } = request.nextUrl;
+  //     const url = new URL(
+  //       `${origin}/login?redirectUrl=${encodeURIComponent(
+  //         `${href.replace(origin, "")}`
+  //       )}`
+  //     );
+  //     return NextResponse.redirect(url);
+  //   }
+  // }
 
   return undefined;
 }
